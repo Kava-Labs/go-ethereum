bf7f38281c6f452a4c094578d2c2429f18b19978
Vladyslav Budichenko <boodyvo@gmail.com>
2024-10-17 10:05:37 -0400

    wip: upgrade kava to sdk v0.50

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -153,7 +153,7 @@ type EVM struct {
 
 // NewEVM returns a new EVM. The returned EVM is not thread safe and should
 // only ever be used *once*.
-func NewEVM(blockCtx BlockContext, txCtx TxContext, statedb StateDB, chainConfig *params.ChainConfig, config Config, customPrecompiles PrecompiledContracts) *EVM {
+func NewEVM(blockCtx BlockContext, txCtx TxContext, statedb StateDB, chainConfig *params.ChainConfig, config Config) *EVM {
 	return NewEVMWithEnabledPrecompiles(blockCtx, txCtx, statedb, chainConfig, config, nil)
 }
 
